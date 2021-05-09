f52691d0346d40cf62b90ae8c9bb1c697ec1aeeb
Matt Thorning <matthewthorning@gmail.com>
2021-05-09 20:28:04 +0100

    change dashboard icons

--- lua/lv-dashboard/init.lua
+++ lua/lv-dashboard/init.lua
@@ -5,10 +5,10 @@ vim.g.dashboard_default_executive = 'telescope'
 
 vim.g.dashboard_custom_section = {
     a = {description = {'  Recent Files       '}, command = 'Telescope oldfiles'},
-    b = {description = {'  Find File          '}, command = 'Telescope find_files'},
+    b = {description = {'  Find File          '}, command = 'Telescope find_files'},
     c = {description = {'  Find Word          '}, command = 'Telescope live_grep'},
     d = {description = {'  Load Last Session  '}, command = 'SessionLoad'},
-    e = {description = {'  New                '}, command = ':enew'},
+    e = {description = {'  New                '}, command = ':enew'},
     f = {description = {'  Settings           '}, command = ':e ~/.config/nvim/lv-settings.lua'},
 }
 

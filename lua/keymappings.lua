93311b2546929d7bec9546ff4ce49fc02241b535
Matt Thorning <matthewthorning@gmail.com>
2021-05-10 21:25:09 +0100

    fixed terminal escape

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -7,19 +7,6 @@ vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {silent = true})
 vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {silent = true})
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {silent = true})
 
--- Terminal window navigation
-vim.cmd([[
-  tnoremap <C-h> <C-\><C-N><C-w>h
-  tnoremap <C-j> <C-\><C-N><C-w>j
-  tnoremap <C-k> <C-\><C-N><C-w>k
-  tnoremap <C-l> <C-\><C-N><C-w>l
-  inoremap <C-h> <C-\><C-N><C-w>h
-  inoremap <C-j> <C-\><C-N><C-w>j
-  inoremap <C-k> <C-\><C-N><C-w>k
-  inoremap <C-l> <C-\><C-N><C-w>l
-  tnoremap <Esc> <C-\><C-n>
-]])
-
 -- resize with arrows
 vim.cmd([[
   nnoremap <silent> <C-Up>    :resize -2<CR>
@@ -28,15 +15,15 @@ vim.cmd([[
   nnoremap <silent> <C-Right> :vertical resize -2<CR>
 ]])
 
--- better indenting
-vim.api.nvim_set_keymap('v', '<', '<gv', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', '>', '>gv', {noremap = true, silent = true})
-
 -- Tab switch
 vim.api.nvim_set_keymap('n', '<TAB>', 'gt', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<S-TAB>', 'gT', {noremap = true, silent = true})
 
 -- Move selected line / block of text in visual mode
-vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('x', 'J', ':move \'>+1<CR>gv-gv', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv', {noremap = true, silent = true})
+
+-- better indenting in visual mode
+vim.api.nvim_set_keymap('v', '<', '<gv', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '>', '>gv', {noremap = true, silent = true})
 

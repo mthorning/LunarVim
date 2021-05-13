4dc30bf4384a907fb99ba6be2e949beeb91ed231
Matt Thorning <matthewthorning@gmail.com>
2021-05-13 08:01:54 +0100

    mall update to header asci art

--- lua/lv-neomux/init.lua
+++ lua/lv-neomux/init.lua
@@ -2,3 +2,8 @@ vim.g.neomux_start_term_map = "<Leader>tt"
 vim.g.neomux_exit_term_mode_map = "<C-space>"
 vim.g.neomux_start_term_split_map = "<Leader>ts"
 vim.g.neomux_start_term_vsplit_map = "<Leader>tv"
+
+if vim.fn.has('nvim') then
+  vim.api.nvim_set_var('$GIT_EDITOR', 'nvr -cc split --remote-wait')
+end
+
